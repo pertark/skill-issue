1f1bec7bbd979cfd1eb0d726c9bd283a0f9a6bce
Patrick Zhang <zhangpatrick2004@gmail.com>
2022-01-06 09:03:07 -0500

    security

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
       </Head>
       { !!currFont ? <Global styles={`
         @font-face {
